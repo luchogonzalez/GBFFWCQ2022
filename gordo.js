58fa9f32848a6c0458bbd9142fc89140552251f5
Lucho Gonzalez <lucho.gonzalez125@gmail.com>
2022-12-11 19:13:47 -0300

    Add Croatia game

--- gordo.js
+++ gordo.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 const PORT = process.env.PORT;
 const MATCH = process.env.MATCH;
 const matchNames = new Map([
-    ['101437163862', 'ARABIA'],
+    ['101437163915', 'ARABIA'],
     ['101437163878', 'MEXICO'],
     ['101437163893', 'POLONIA'],
     ['101437163904', '8VOS'],
@@ -61,7 +61,7 @@ checkAndAddTickets = async function (page) {
 }
 
 browserGoTo = async (page) => {
-    await page.goto(`https://resale-intl.fwc22.tickets.fifa.com/secured/selection/resale/item?performanceId=${MATCH}&productId=101437163862&lang=es`,
+    await page.goto(`https://resale-intl.fwc22.tickets.fifa.com/secured/selection/resale/item?performanceId=${MATCH}&productId=101397570845&lang=es`,
         {
             waitUntil: 'load',
             timeout: 0
